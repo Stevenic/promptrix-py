f73ec7a24fac991f0b6273d1810184df199f1615
bdambrosio <bruce.dambrosio@gmail.com>
2023-06-04 20:47:57 -0700

    rearranged directory structure for pypi

--- example.py
+++
@@ -1,8 +0,0 @@
-import Prompt, SystemMessage, UserMessage
-
-prompt = Prompt.Prompt([
-    SystemMessage.SystemMessage('The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.'),
-    UserMessage.UserMessage('{{$input}}')
-])
-
-print(prompt.getLayoutLength())
