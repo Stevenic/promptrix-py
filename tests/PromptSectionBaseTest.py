cde7d016e69b8aa2ad377cba32c205dde77348a2
Zbigniew Lukasiak <zzbbyy@gmail.com>
2023-11-05 16:14:46 +0100

    async removed from tests; fixed README example

--- tests/PromptSectionBaseTest.py
+++ tests/PromptSectionBaseTest.py
@@ -1,4 +1,4 @@
-import aiounittest, unittest
+import unittest
 from promptrix.promptrixTypes import *
 from promptrix.PromptSectionBase import PromptSectionBase
 from promptrix.VolatileMemory import VolatileMemory
@@ -6,15 +6,15 @@ from promptrix.FunctionRegistry import FunctionRegistry
 from promptrix.GPT3Tokenizer import GPT3Tokenizer
 
 class TestSection(PromptSectionBase):
-    async def renderAsMessages(self, memory: PromptMemory, functions: PromptFunctions, tokenizer: Tokenizer, max_tokens: int):
+    def renderAsMessages(self, memory: PromptMemory, functions: PromptFunctions, tokenizer: Tokenizer, max_tokens: int):
         return self.return_messages([{'role': 'test', 'content': 'Hello Big World'}], 3, tokenizer, max_tokens)
 
 
 class MultiTestSection(PromptSectionBase):
-    async def renderAsMessages(self, memory: PromptMemory, functions: PromptFunctions, tokenizer: Tokenizer, max_tokens: int):
+    def renderAsMessages(self, memory: PromptMemory, functions: PromptFunctions, tokenizer: Tokenizer, max_tokens: int):
         return self.return_messages([{'role': 'test', 'content': 'Hello Big'}, {'role': 'test', 'content': 'World'}], 3, tokenizer, max_tokens)
 
-class TestPromptSectionBase(aiounittest.AsyncTestCase):
+class TestPromptSectionBase(unittest.TestCase):
     def setUp(self):
         self.memory = VolatileMemory()
         self.functions = FunctionRegistry()
@@ -27,46 +27,46 @@ class TestPromptSectionBase(aiounittest.AsyncTestCase):
         self.assertEqual(section.separator, "\n")
         self.assertEqual(section.text_prefix, "")
 
-    async def test_renderAsMessages(self):
+    def test_renderAsMessages(self):
         section = TestSection()
-        rendered = await section.renderAsMessages(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsMessages(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, [{'role': 'test', 'content': 'Hello Big World'}])
         self.assertEqual(rendered.length, 3)
         self.assertEqual(rendered.tooLong, False)
 
         section = TestSection(2)
-        rendered = await section.renderAsMessages(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsMessages(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, [{'role': 'test', 'content': 'Hello Big'}])
         self.assertEqual(rendered.length, 2)
         self.assertEqual(rendered.tooLong, False)
 
         section = TestSection(2)
-        rendered = await section.renderAsMessages(self.memory, self.functions, self.tokenizer, 1)
+        rendered = section.renderAsMessages(self.memory, self.functions, self.tokenizer, 1)
         self.assertEqual(rendered.output, [{'role': 'test', 'content': 'Hello Big'}])
         self.assertEqual(rendered.length, 2)
         self.assertEqual(rendered.tooLong, True)
 
         section = MultiTestSection(2)
-        rendered = await section.renderAsMessages(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsMessages(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, [{'role': 'test', 'content': 'Hello Big'}])
         self.assertEqual(rendered.length, 2)
         self.assertEqual(rendered.tooLong, False)
 
-    async def test_renderAsText(self):
+    def test_renderAsText(self):
         section = TestSection()
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, "Hello Big World")
         self.assertEqual(rendered.length, 3)
         self.assertEqual(rendered.tooLong, False)
 
         section = TestSection(4, True, "\n", "user: ")
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, "user: Hello Big")
         self.assertEqual(rendered.length, 4)
         self.assertEqual(rendered.tooLong, False)
 
         section = TestSection(4, True, "\n", "user: ")
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 1)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 1)
         self.assertEqual(rendered.output, "user: Hello Big")
         self.assertEqual(rendered.length, 4)
         self.assertEqual(rendered.tooLong, True)
--- tests/TemplateSectionTest.py
+++ tests/TemplateSectionTest.py
@@ -3,7 +3,6 @@ from promptrix.TemplateSection import TemplateSection
 from promptrix.VolatileMemory import VolatileMemory
 from promptrix.FunctionRegistry import FunctionRegistry
 from promptrix.GPT3Tokenizer import GPT3Tokenizer
-import asyncio
 
 class TestTemplateSection(unittest.TestCase):
     def setUp(self):
@@ -32,71 +31,71 @@ class TestTemplateSection(unittest.TestCase):
         self.assertEqual(section.required, False)
         self.assertEqual(section.separator, "\n")
 
-    async def test_renderAsMessages(self):
+    def test_renderAsMessages(self):
         section = TemplateSection("Hello World", "user")
-        rendered = await section.renderAsMessages(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsMessages(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, [{'role': 'user', 'content': 'Hello World'}])
         self.assertEqual(rendered.length, 2)
         self.assertEqual(rendered.tooLong, False)
 
         section = TemplateSection("Hello World", "user")
-        rendered = await section.renderAsMessages(self.memory, self.functions, self.tokenizer, 1)
+        rendered = section.renderAsMessages(self.memory, self.functions, self.tokenizer, 1)
         self.assertEqual(rendered.output, [{'role': 'user', 'content': 'Hello World'}])
         self.assertEqual(rendered.length, 2)
         self.assertEqual(rendered.tooLong, True)
 
-    async def test_renderAsText(self):
+    def test_renderAsText(self):
         section = TemplateSection("Hello World", "user")
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, "Hello World")
         self.assertEqual(rendered.length, 2)
         self.assertEqual(rendered.tooLong, False)
 
         section = TemplateSection("Hello World", "user")
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 1)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 1)
         self.assertEqual(rendered.output, "Hello World")
         self.assertEqual(rendered.length, 2)
         self.assertEqual(rendered.tooLong, True)
 
-    async def test_template_syntax(self):
+    def test_template_syntax(self):
         section = TemplateSection("Hello {{$foo}}", "user")
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, "Hello bar")
         self.assertEqual(rendered.length, 2)
         self.assertEqual(rendered.tooLong, False)
 
         section = TemplateSection("Hello {{$foo}} {{test}}", "user")
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, "Hello bar Hello World")
         self.assertEqual(rendered.length, 4 )
         self.assertEqual(rendered.tooLong, False)
 
         section = TemplateSection("Hello {{test2 World}}", "user")
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, "Hello World")
         self.assertEqual(rendered.length, 2)
         self.assertEqual(rendered.tooLong, False)
 
         section = TemplateSection("Hello {{test2 'Big World'}}", "user")
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, "Hello Big World")
         self.assertEqual(rendered.length, 3)
         self.assertEqual(rendered.tooLong, False)
 
         section = TemplateSection("Hello {{test2 `Big World`}}", "user")
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, "Hello Big World")
         self.assertEqual(rendered.length, 3)
         self.assertEqual(rendered.tooLong, False)
 
         section = TemplateSection("Hello {{test3 'Big' World}}", "user")
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, "Hello Big World")
         self.assertEqual(rendered.length, 3)
         self.assertEqual(rendered.tooLong, False)
 
         section = TemplateSection("{{}}", "user")
-        rendered = await section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
+        rendered = section.renderAsText(self.memory, self.functions, self.tokenizer, 100)
         self.assertEqual(rendered.output, "")
         self.assertEqual(rendered.length, 0)
         self.assertEqual(rendered.tooLong, False)
@@ -109,12 +108,7 @@ class TestTemplateSection(unittest.TestCase):
             section = TemplateSection("Hello {{test3 'Big}}", "user")
             self.assertTrue('Invalid template: Hello {{test3 \'Big}}' in str(context.exception))
 
-ts = TestTemplateSection()
-ts.setUp()
-ts.test_constructor()
-
 if __name__ == '__main__':
-    asyncio.run(ts.test_renderAsMessages())
-    asyncio.run(ts.test_renderAsText())
-    asyncio.run(ts.test_template_syntax())
-    
+    unittest.main()
+
+
